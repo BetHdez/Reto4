16ef6ad22893b1cdc4476155e293add6b128fb45
Bet <145491864+BetHdez@users.noreply.github.com>
2024-07-03 02:32:14 -0600

    Update app.py

--- mini_proyectos_python/numero_palabra/app.py
+++ mini_proyectos_python/numero_palabra/app.py
@@ -1,1 +1,20 @@
+from flask import Flask, render_template, request
+from num2words import num2words
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    num_palabra = ""
+    if request.method == 'POST':
+        num = request.form.get('num')
+        try:
+            num = int(num)
+            num_palabra = num2words(num, lang='es')
+        except ValueError:
+            num_palabra = "Por favor, introduce un número válido."
+    return render_template('index.html', num_palabra=num_palabra)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
